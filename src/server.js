2158e0ecaca2261557f5ac2a795ba7a9e0359f54
Shi Tong Yuan <z5192519@ad.unsw.edu.au>
2023-07-12 20:37:25 +1000

    创建docker-compose.yml

--- src/server.js
+++ src/server.js
@@ -265,7 +265,7 @@ const port = process.env.PORT || 5005;
 
 const server = app.listen(port, '0.0.0.0', () => {
   console.log(`Backend is now listening on port ${port}!`);
-  console.log(`For API docs, navigate to http://localhost:${port}`);
+  console.log(`For API docs, navigate to http://${process.env.IP}:${port}`);
 });
 
 export default server;
